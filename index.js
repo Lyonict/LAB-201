4c81e4f66b577ceb0b068c9207e533ecf8e4e69c
Lyonict <charles42.h@gmail.com>
2022-03-18 16:48:01 +0100

    primary hosting

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyyd9RZzxT7XOIQNHCqiwBOyfdgFnImFA",
+  authDomain: "vald-88337.firebaseapp.com",
+  projectId: "vald-88337",
+  storageBucket: "vald-88337.appspot.com",
+  messagingSenderId: "539960034339",
+  appId: "1:539960034339:web:c4a4b85f22406878115d96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
