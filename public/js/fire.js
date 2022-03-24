6b9ef5b0c367ba1e3b1a4e8c3eaffac99f57235c
Lyonict <charles42.h@gmail.com>
2022-03-24 11:36:19 +0100

    Avant install React

--- public/js/fire.js
+++ public/js/fire.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "../../node_modules/firebase/firebase-app.js";
+import { getAuth } from "../../node_modules/firebase/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
